ed8944bba77de1785bfdfcd22aab6d04b97e5ef5
pietrobolcato <pietrobolcato@gmail.com>
2022-10-12 11:48:01 +0000

    chore: write documentation for sampler callback

--- animation/generator/util.py
+++ animation/generator/util.py
@@ -1,11 +1,11 @@
-import os
-import time
+# -*- coding: utf-8 -*-
+"""This module defines the helpers function for the animation computation"""
+
 import torch
 import cv2
 import math
 import numpy as np
 import requests
-import torchvision.transforms.functional as TF
 import animation.pytorch3d_lite.py3d_tools as p3d
 import random
 import animation.generator.prompt_weighting as prompt_weighting
@@ -15,7 +15,15 @@ from PIL import Image
 
 
 def load_img(path, shape):
-    # use_alpha_as_mask: Read the alpha channel of the image as the mask image
+    """Load image either from an url, or a local file, and return it as a tensor of a specified shape
+
+    Args:
+        path (str): the url or path of the image file
+        shape (tuple of int, int): the shape of the tensor to return
+
+    Returns:
+        tensor: the image as a tensor of the specified shape"""
+
     if path.startswith("http://") or path.startswith("https://"):
         image = Image.open(requests.get(path, stream=True).raw)
     else:
@@ -33,42 +41,55 @@ def load_img(path, shape):
 
 
 def anim_frame_warp_3d(prev_img_cv2, depth, anim_args, keys, frame_idx, device):
-    TRANSLATION_SCALE = 1.0 / 200.0  # matches Disco
+    """Warp the animation frame in 3D space"""
+
+    TRANSLATION_SCALE = 1.0 / 200.0
+
     translate_xyz = [
         -keys.translation_x_series[frame_idx] * TRANSLATION_SCALE,
         keys.translation_y_series[frame_idx] * TRANSLATION_SCALE,
         -keys.translation_z_series[frame_idx] * TRANSLATION_SCALE,
     ]
+
     rotate_xyz = [
         math.radians(keys.rotation_3d_x_series[frame_idx]),
         math.radians(keys.rotation_3d_y_series[frame_idx]),
         math.radians(keys.rotation_3d_z_series[frame_idx]),
     ]
+
     rot_mat = p3d.euler_angles_to_matrix(
         torch.tensor(rotate_xyz, device=device), "XYZ"
     ).unsqueeze(0)
+
     result = transform_image_3d(
         prev_img_cv2, depth, rot_mat, translate_xyz, anim_args, device
     )
     torch.cuda.empty_cache()
+
     return result
 
 
 def transform_image_3d(
     prev_img_cv2, depth_tensor, rot_mat, translate, anim_args, device
 ):
-    # adapted and optimized version of transform_image_3d from Disco Diffusion https://github.com/alembics/disco-diffusion
+    """
+    Transform image in 3D space
+    Adapted and optimized from Disco Diffusion https://github.com/alembics/disco-diffusion"""
+
     w, h = prev_img_cv2.shape[1], prev_img_cv2.shape[0]
 
     aspect_ratio = float(w) / float(h)
+
     near, far, fov_deg = (
         anim_args["near_plane"],
         anim_args["far_plane"],
         anim_args["fov"],
     )
+
     persp_cam_old = p3d.FoVPerspectiveCameras(
         near, far, aspect_ratio, fov=fov_deg, degrees=True, device=device
     )
+
     persp_cam_new = p3d.FoVPerspectiveCameras(
         near,
         far,
@@ -85,6 +106,7 @@ def transform_image_3d(
         torch.linspace(-1.0, 1.0, h, dtype=torch.float32, device=device),
         torch.linspace(-1.0, 1.0, w, dtype=torch.float32, device=device),
     )
+
     z = torch.as_tensor(depth_tensor, dtype=torch.float32, device=device)
     xyz_old_world = torch.stack((x.flatten(), y.flatten(), z.flatten()), dim=1)
 
@@ -124,17 +146,22 @@ def transform_image_3d(
         .numpy()
         .astype(prev_img_cv2.dtype)
     )
+
     return result
 
 
-def sample_from_cv2(sample: np.ndarray) -> torch.Tensor:
+def sample_from_cv2(sample: np.ndarray):
+    """Sample from cv2 to torch tensor"""
+
     sample = ((sample.astype(float) / 255.0) * 2) - 1
     sample = sample[None].transpose(0, 3, 1, 2).astype(np.float16)
     sample = torch.from_numpy(sample)
     return sample
 
 
-def sample_to_cv2(sample: torch.Tensor, type=np.uint8) -> np.ndarray:
+def sample_to_cv2(sample, type=np.uint8):
+    """Sample from torch tensor to cv2"""
+
     sample_f32 = rearrange(sample.squeeze().cpu().numpy(), "c h w -> h w c").astype(
         np.float32
     )
@@ -144,6 +171,17 @@ def sample_to_cv2(sample: torch.Tensor, type=np.uint8) -> np.ndarray:
 
 
 def maintain_colors(prev_img, color_match_sample, mode):
+    """Tries to match color of previous frame
+
+    Args:
+      prev_img (Image): the previous generated frame
+      color_match_sample (Image): the first generated frame
+      mode (str): can be "Match Frame 0 RGB", "Match Frame 0 HSV", or "Match Frame 0 LAB"
+                  the algorithm will try to minimize the distance in the selected color space and mode
+
+    Returns:
+      Image: image with color coherence
+    """
     if mode == "Match Frame 0 RGB":
         return match_histograms(prev_img, color_match_sample, multichannel=True)
     elif mode == "Match Frame 0 HSV":
@@ -158,22 +196,29 @@ def maintain_colors(prev_img, color_match_sample, mode):
         return cv2.cvtColor(matched_lab, cv2.COLOR_LAB2RGB)
 
 
-def add_noise(sample: torch.Tensor, noise_amt: float) -> torch.Tensor:
+def add_noise(sample, noise_amt):
+    """Add noise to a sample"""
+
     return sample + torch.randn(sample.shape, device=sample.device) * noise_amt
 
 
 def next_seed(args):
+    """Changes seed based on behavior defined in args"""
+
     if args.seed_behavior == "iter":
         args.seed += 1
     elif args.seed_behavior == "fixed":
-        pass  # always keep seed the same
+        pass
     else:
         args.seed = random.randint(0, 2**32 - 1)
+
     return args.seed
 
 
 def get_uc_and_c(prompts, model, args, frame=0):
-    prompt = prompts[0]  # they are the same in a batch anyway
+    """Returns uc and c for weighted prompts"""
+
+    prompt = prompts[0]
 
     # get weighted sub-prompts
     (
