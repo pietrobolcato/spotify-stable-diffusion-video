2c4cab9625e0e2c8e87c959ecbce82a2c2b1fd77
pietrobolcato <pietrobolcato@gmail.com>
2022-11-22 17:07:42 +0000

    chore: extended documentation

--- util.py
+++ util.py
@@ -18,7 +18,13 @@ def init_logging(logging_level=logging.INFO):
 
 
 def get_global_settings(settings_path="global_settings.yaml"):
-    """Get global settings from specified .yaml file"""
+    """
+    Get global settings from specified .yaml file
+
+    The yaml file can use the special token $CURRENT_FOLDER$ to specify
+    path relatives to the current directory
+
+    """
 
     assert os.path.exists(
         settings_path
@@ -26,5 +32,12 @@ def get_global_settings(settings_path="global_settings.yaml"):
 
     with open(settings_path) as f:
         data = yaml.load(f, Loader=SafeLoader)
+
+    data["models_path"] = data["models_path"].replace(
+        "$CURRENT_FOLDER$", os.path.dirname(os.path.realpath(__file__))
+    )
+    data["output_directory"] = data["output_directory"].replace(
+        "$CURRENT_FOLDER$", os.path.dirname(os.path.realpath(__file__))
+    )
 
     return data
